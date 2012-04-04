ab428f1d17319f1993241c47f59305d530c8447b
Espen A. Kristiansen <post@espenak.net>
2012-04-04 08:55:08 +0200

    Better debugging.

--- addexampledata/management/commands/addexampledata.py
+++ addexampledata/management/commands/addexampledata.py
@@ -1,7 +1,6 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 from django.utils.importlib import import_module
-
 
 class Command(BaseCommand):
     args = '[myapp] [another_app] ...'
@@ -9,6 +8,11 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         appnames = args
+        verbosity = int(options.get('verbosity', '2'))
+
+        for appname in appnames:
+            if not appname in settings.INSTALLED_APPS:
+                raise CommandError('App not in INSTALLED_APPS: {0}'.format(appname))
         for appname in settings.INSTALLED_APPS:
             if appnames and not appname in appnames:
                 continue
@@ -16,6 +20,13 @@ class Command(BaseCommand):
             try:
                 mod = import_module(modulepath)
             except ImportError, e:
-                continue
+                if appname in appnames:
+                    raise CommandError('Could not import {0}'.format(modulepath))
+                else:
+                    if verbosity > 1:
+                        print '{0} not found'.format(modulepath)
+                    continue
             else:
+                if verbosity > 0:
+                    print 'Adding exampledata from:', modulepath
                 mod.add_exampledata()
